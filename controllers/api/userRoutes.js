051012d4bd83d89ba9329fdb8ccf9290bf20dc71
Evan Chang <evanchang17812@hotmail.com>
2024-01-24 21:49:10 -0500

    Login and signup now works

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,8 +4,7 @@ const { User } = require('../../models');
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create({
-      username: req.body.username,
-      email: req.body.email,
+      name: req.body.name,
       password: req.body.password,
     });
 
