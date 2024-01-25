f0ce55030c17f8eaf61fea0658d986498fd95a3a
Evan Chang <evanchang17812@hotmail.com>
2024-01-24 19:46:55 -0500

    fixing controllers, models, and seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,18 +1,27 @@
 const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
-
-const recipeData = require('./recipeData.json');
+const userData = require('./userData.json');
+//const recipeData = require('./recipeData.json');
 
 const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
 
-  for (const recipe of recipeData) {
-    await Recipe.create({
-      ...recipe,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+
+
+  // for (const recipe of recipeData) {
+  //   await Recipe.create({
+  //     ...recipe,
+  //     user_id: users[Math.floor(Math.random() * users.length)].id,
+  //   });
+  // }
 
   process.exit(0);
 };
 
 seedDatabase();
+
