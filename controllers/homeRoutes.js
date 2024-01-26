4d6c56f83e96e0fb8283b1d3810e82cf78d085a9
rosiedeng1 <rd25500@gmail.com>
2024-01-25 22:38:01 -0500

    changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Recipe } = require('../models');
+const { Recipe, User } = require('../models');
 // Import the custom middleware
 const withAuth = require('../utils/auth');
 
